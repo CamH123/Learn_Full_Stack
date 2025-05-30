e59850c38d83a769efc46179fd53ffce96ee0861
CamH123 <Cameron.h.1732@gmail.com>
2025-05-30 10:49:32 -0500

    add lean js section

---
+++ javascript-course/learnjs1.js
@@ -0,0 +1,22 @@
+let name = "john";
+let surname = "doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+age  =54;
+console.log(age)
+
+const pi = 3.14;
+// pi = 10
+console.log(pi)
+console.log((3+2)-76*(1+1))
+
+console.log(name[0])
+alert("hi you")
+
+if (name.length > surname.length){
+    console.log("first bigger than second".substring(0, 5))
+}
